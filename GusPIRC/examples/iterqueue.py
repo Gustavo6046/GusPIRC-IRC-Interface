08de5f12d38d9a1961c5f804b3cfdba004b2d2ef
Gustavo Ramos Rehermann <gugurehermann@gmail.com>
2016-03-21 15:11:15 -0300

    Adding Iterable Queue (example GusBot requirement)

---
+++ GusPIRC/examples/iterqueue.py
@@ -0,0 +1,13 @@
+# Special thanks to StackOverflow user RaymondHettinger for contributing this solution! :D
+
+from Queue import Queue
+
+class IterableQueue(Queue):
+
+    _sentinel = object()
+
+    def __iter__(self):
+        return iter(self.get, self._sentinel)
+
+    def close(self):
+        self.put(self._sentinel)
